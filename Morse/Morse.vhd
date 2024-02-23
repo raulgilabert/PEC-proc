3d0500c9bdb1ed2ebb57492ebe8cb2db2580fc32
Raúl Gilabert <raulgilabert@proton.me>
2024-02-23 10:17:20 +0100

    commit 1

--- Morse/Morse.vhd
+++ Morse/Morse.vhd
@@ -35,47 +35,16 @@ ARCHITECTURE Structure OF Morse IS
 	END COMPONENT;
 	
 	
-	COMPONENT MorseChar IS
-		GENERIC (
-			size: integer
-		);
-		PORT (
-			data: IN std_logic_vector(size - 1 downto 0);
-			reset: IN std_logic;
-			clk: IN std_logic;
-			led: OUT std_logic;
-			end_char: OUT std_logic
-		);
-	END COMPONENT;
+--	COMPONENT MorseChar IS
+--		PORT (
+      
+--		);
+--	END COMPONENT;
 	
+  SIGNAL reset: std_logic;
+  SIGNAL clk_05: std_logic;
 
-	SIGNAL clk_05: std_logic := '0';
-	SIGNAL clk_15: std_logic := '0';
-	SIGNAL reset: std_logic := '0';
-	SIGNAL reset_s: std_logic := '0';
-	SIGNAL A_s: std_logic;
-	SIGNAL B_s: std_logic;
-	SIGNAL C_s: std_logic;
-	SIGNAL D_s: std_logic;
-	SIGNAL E_s: std_logic;
-	SIGNAL F_s: std_logic;
-	SIGNAL G_s: std_logic;
-	SIGNAL H_s: std_logic;
-	SIGNAL end_bit: std_logic;
-	SIGNAL end_bitA: std_logic;
-	SIGNAL end_bitB: std_logic;
-	SIGNAL end_bitC: std_logic;
-	SIGNAL end_bitD: std_logic;
-	SIGNAL end_bitE: std_logic;
-	SIGNAL end_bitF: std_logic;
-	SIGNAL end_bitG: std_logic;
-	SIGNAL end_bitH: std_logic;
-	
-	
 BEGIN
-	
-	--ledG(0) <= '1';
-	
 	
 	Clock_05: clock
 		GENERIC map(
@@ -88,17 +57,6 @@ BEGIN
 			clk => clk_05
 		);
 		
-	Clock_15: clock
-		GENERIC map(
-			27
-		)
-		PORT map(
-			CLOCK_50 => CLOCK_50,
-			reset => reset,
-			counter => std_logic_vector(to_unsigned(75_000_000, 27)),
-			clk => clk_15
-		);
-
 	
 	Visualizer: driver7display
 		PORT map(
@@ -106,144 +64,7 @@ BEGIN
 			charBits => HEX0
 		);
 		
-	A: MorseChar
-		GENERIC map(
-			6
-		)
-		PORT map(
-			data => "101110",
-			reset => reset,
-			clk => clk_05,
-			led => A_s,
-			end_char => end_bitA
-		);
-
-	B: MorseChar
-		GENERIC map(
-			10
-		)
-		PORT map(
-			data => "1110101010",
-			reset => reset,
-			clk => clk_05,
-			led => B_s,
-			end_char => end_bitB
-		);
-
-	C: MorseChar
-		GENERIC map(
-			12
-		)
-		PORT map(
-			data => "111010111010",
-			reset => reset,
-			clk => clk_05,
-			led => C_s,
-			end_char => end_bitC
-		);
-
-	D: MorseChar
-		GENERIC map(
-			8
-		)
-		PORT map(
-			data => "11101010",
-			reset => reset,
-			clk => clk_05,
-			led => D_s,
-			end_char => end_bitD
-		);
-
-	E: MorseChar
-		GENERIC map(
-			2
-		)
-		PORT map(
-			data => "10",
-			reset => reset,
-			clk => clk_05,
-			led => E_s,
-			end_char => end_bitE
-		);
-
-	F: MorseChar
-		GENERIC map(
-			10
-		)
-		PORT map(
-			data => "1010111010",
-			reset => reset,
-			clk => clk_05,
-			led => F_s,
-			end_char => end_bitF
-		);
-
-	G: MorseChar
-		GENERIC map(
-			10
-		)
-		PORT map(
-			data => "1110111010",
-			reset => reset,
-			clk => clk_05,
-			led => G_s,
-			end_char => end_bitG
-		);
-
-	H: MorseChar
-		GENERIC map(
-			8
-		)
-		PORT map(
-			data => "10101010",
-			reset => reset,
-			clk => clk_05,
-			led => H_s,
-			end_char => end_bitH
-		);
-		
-	reset <= not key(0) or not key(1);
-
---	with sw select
---		ledR(0) <= A_s when "000",
---					  B_s when "001", 
---					  C_s when "010",
---					  D_s when "011",
---					  E_s when "100",
---					  F_s when "101",
---					  G_s when "110",
---					  H_s when others;
-
-	with sw select	
-		end_bit <= end_bitA when "000",
-				   end_bitB	when "001",
-				   end_bitC when "010",
-				   end_bitD when "011",
-				   end_bitE when "100",
-				   end_bitF when "101",
-				   end_bitG when "110",
-				   end_bitH when others;
-				   
-
-	
-	
-	with end_bit & sw select
-		ledR(0) <= A_s when "0000",
-					  B_s when "0001", 
-					  C_s when "0010",
-					  D_s when "0011",
-					  E_s when "0100",
-					  F_s when "0101",
-					  G_s when "0110",
-					  H_s when "0111",
-					  '0' when others;
-
-	
-	ledG(0) <= not end_bit;
-
-	-- temp
-	ledg(1) <= clk_05;
-	ledg(2) <= clk_15;
+    
 
 END Structure;
 
--- Morse/MorseChar.vhd
+++ Morse/MorseChar.vhd
@@ -9,47 +9,46 @@ use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
 
 ENTITY MorseChar IS
-	GENERIC (
-		size: integer
-	);
-	PORT (
-		data: IN std_logic_vector(size - 1 downto 0);
-		reset: IN std_logic;
-		clk: IN std_logic;
-		led: OUT std_logic;
-		end_char: OUT std_logic
-	);
+  PORT (
+    clk: IN std_logic;
+    reset: IN std_logic;
+    data: IN std_logic_vector(12 DOWNTO 0); -- 12 bits máximo de datos termina con 00
+    led_data: OUT std_logic;
+    end_char: OUT std_logic
+  );
 END MorseChar;
 
 
 ARCHITECTURE Structure OF MorseChar IS
-	SIGNAL counter: std_logic_vector(3 downto 0);
-	SIGNAL counter_s: std_logic_vector(3 downto 0);
+
+  SIGNAL bit_counter: std_logic_vector(3 downto 0);
+  SIGNAL bit_counter_s: std_logic_vector(3 downto 0);
+  SIGNAL end_char_s: std_logic;
 
 BEGIN
-	
-	PROCESS (clk)
-	BEGIN
-		if rising_edge(clk) then
-			counter_s <= counter - 1;
-			
-			led <= data(to_integer(unsigned(counter)));
-			
-			end_char <= '0';
-			
-			if counter = 0 then
-				counter_s <= std_logic_vector(to_unsigned(size - 1, 4));
-				end_char <= '1';
-			END if;
-		END if;
-		
-		
-		
-	END PROCESS;
-		
-	with reset select 	
-			counter <= counter_s when '0',
-						  (others=>'0') when others;
+  PROCESS (clk)
+  BEGIN
+    if rising_edge(clk) then
+      bit_counter_s <= bit_counter + 1;
+    END if;
+  END PROCESS;
+  
+  -- si el bit anterior y este es 0 es fin de dato
+  end_char_s <= data(to_integer(unsigned(bit_counter)) - 1) nor
+                data(to_integer(unsigned(bit_counter)));
+
+  -- se muestra el dato si es 1 en el vector (en morse se controla si se puede mostrar)
+  led_data <= data(to_integer(unsigned(bit_counter)));
+
+  -- si se resetea o es fin de dato el contador se pone a 0
+  with (0=> reset, 1=> end_char_s) select
+    bit_counter <= bit_counter_s when "00",
+                   "0000" when others;
+
+
+  with reset select
+    end_char  <= '0' when '1',
+                 end_char_s when others;
 	
 END Structure;
 
