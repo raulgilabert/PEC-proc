52af6dc3df23ec4889ddc113cd1526f9c49e9f23
polsahi <psaumell100@gmail.com>
2024-02-26 11:31:47 +0100

    EUREKA!

--- Morse/Morse.vhd
+++ Morse/Morse.vhd
@@ -40,8 +40,7 @@ ARCHITECTURE Structure of Morse IS
 	COMPONENT MorseChar IS
 		PORT (
 			clk: IN std_logic;
-			data: IN std_logic_vector(11 downto 0);
-			len: IN std_logic_vector(3 downto 0);
+			data: IN std_logic_vector(12 downto 0);
 			working: IN std_logic;
 			led: OUT std_logic;
 			end_char: OUT std_logic
@@ -54,45 +53,30 @@ ARCHITECTURE Structure of Morse IS
 	SIGNAL clk: std_logic;
 	
 	SIGNAL end_char: std_logic;
-	
-	SIGNAL len: std_logic_vector(3 downto 0);
-	SIGNAL data: std_logic_vector(11 downto 0);
-	
+	SIGNAL data: std_logic_vector(12 downto 0);
 	SIGNAL state: std_logic := '0'; -- 0->repos, 1->morse
 	
-	SIGNAL actual_s: std_logic_vector (3 downto 0):= "0000";
 	SIGNAL actual: std_logic_vector (3 downto 0):= "0000";
 	SIGNAL s: std_logic_vector (3 downto 0) := "0000";
 	SIGNAL str: std_logic_vector (3 downto 0) := "0000";
+	
 	SIGNAL led: std_logic;
 BEGIN
 
-
 	with SW select
-		len <= "0110" when "000",
-				 "1010" when "001",
-				 "1100" when "010",
-				 "1000" when "011",
-				 "0010" when "100",
-				 "1010" when "101",
-				 "1010" when "110",
-				 "1000" when others;
-
-	with SW select
-		 data <= "101110000000" when "000",
-					"111010101000" when "001",
-					"111010111010" when "010",
-					"111010100000" when "011",
-					"100000000000" when "100",
-					"101011101000" when "101",
-					"111011101000" when "110",
-					"101010100000" when others;
+		 data <= "1011100000000" when "000",
+					"1110101010000" when "001",
+					"1110101110100" when "010",
+					"1110101000000" when "011",
+					"1000000000000" when "100",
+					"1010111010000" when "101",
+					"1110111010000" when "110",
+					"1010101000000" when others;
 
 	morse_char: MorseChar
 		PORT map(
 			clk => clk,
 			data => data,
-			len => len,
 			working => state,
 			led => led,
 			end_char => end_char
@@ -115,30 +99,6 @@ BEGIN
 		);	
 	
 	ledg(0) <= not state;
-
-	--PROCESS (clk)
-	--BEGIN
-	--	if rising_edge(clk) then
-	--		if state = '0' and key(1) = '0' then
-	--			state <= '1';
-	--		elsif state = '1' and end_char = '1' then
-	--			state <= '0';
-	--		elsif key(0) = '0' then
-	--			state <= '0';
-	--		elsif state = '1' and key(0) = '1' then
-	--			state <= '1';
-	--		else
-	--			state <= '0';
-	--		END if;
-	--	end if;
-	--END PROCESS;
-
-	
-	--ledg(2) <= key(0);
-	
-	--ledG(3) <= state;
-	
-	--actual <= s;
 
 	ledr(0) <= s (2);
 
