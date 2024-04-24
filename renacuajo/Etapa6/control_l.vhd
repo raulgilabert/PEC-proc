584a41b4efe0b2ae48f7dcffd409c1d443fe62a9
Pol Saumell Hill <94972560+polsahi@users.noreply.github.com>
2024-04-24 11:20:58 +0200

    fix glitches i refactor amb consts
    he augmentat el package amb constants pels op code i pels f

--- renacuajo/Etapa6/control_l.vhd
+++ renacuajo/Etapa6/control_l.vhd
@@ -37,34 +37,38 @@ ARCHITECTURE Structure OF control_l IS
 BEGIN
 
 	with ir(5 downto 3) select
-		arit_log <= AND_I when "000", -- AND
-					OR_I  WHEN "001", -- OR
-					XOR_I WHEN "010", -- XOR
-					NOT_I WHEN "011", -- NOT
-					ADD_I WHEN "100", -- ADD
-					SUB_I WHEN "101", -- SUB
-					SHA_I WHEN "110", -- SHA
-					SHL_I WHEN others;-- SHL
+		arit_log <= AND_I when F_AND, -- AND
+					OR_I  WHEN F_OR, -- OR
+					XOR_I WHEN F_XOR, -- XOR
+					NOT_I WHEN F_NOT, -- NOT
+					ADD_I WHEN F_ADD, -- ADD
+					SUB_I WHEN F_SUB, -- SUB
+					SHA_I WHEN F_SHA, -- SHA
+					SHL_I WHEN F_SHL,
+					NOP_I WHEN others;-- SHL
 						
 	with ir(5 downto 3) select
-		cmp <= CMPLT_I when "000", -- CMPLT
-			   CMPLE_I WHEN "001", -- CMPLE
-			   CMPEQ_I WHEN "011", -- CMPEQ
-			   CMPLTU_I WHEN "100", -- CMPLTU
-			   CMPLEU_I WHEN others;-- CMPLEU
+		cmp <= CMPLT_I when F_CMPLT, -- CMPLT
+			   CMPLE_I WHEN F_CMPLE, -- CMPLE
+			   CMPEQ_I WHEN F_CMPEQ, -- CMPEQ
+			   CMPLTU_I WHEN F_CMPLTU, -- CMPLTU
+			   CMPLEU_I WHEN F_CMPLEU,-- CMPLEU
+			   NOP_I WHEN others;
 
 	with ir(5 downto 3) select
-		mul_div <= MUL_I when "000", -- MUL
-				   MULH_I WHEN "001", -- MULH
-				   MULHU_I WHEN "010", -- MULHU
-				   DIV_I WHEN "100", -- DIV
-				   DIVU_I WHEN others;-- DIVU
+		mul_div <= MUL_I when F_MUL, -- MUL
+				   MULH_I WHEN F_MULH, -- MULH
+				   MULHU_I WHEN F_MULHU, -- MULHU
+				   DIV_I WHEN F_DIV, -- DIV
+				   DIVU_I WHEN F_DIVU,-- DIVU
+				   NOP_I WHEN others;
 				 
 	with ir(2 downto 0) select
-		jump <= JZ_I when "000", -- JZ
-				JNZ_I when "001", -- JNZ
-				JMP_I when "011", -- JMP
-				JAL_I when others;-- JAL
+		jump <= JZ_I when F_JZ, -- JZ
+				JNZ_I when F_JNZ, -- JNZ
+				JMP_I when F_JMP, -- JMP
+				JAL_I when F_JAL,
+				NOP_I when others;-- JAL
 
 	with ir(8) select
 		move <= MOVI_I when '0', -- MOVI
@@ -80,44 +84,44 @@ BEGIN
 				  
 				  
 	with ir(15 DOWNTO 12) select
-		op <= arit_log when "0000", --ARIT_LOGIC
-			  cmp when "0001", --CMP
-			  ADDI_I when "0010", -- ADDI
-			  LD_I when "0011", --LD
-			  ST_I when "0100", -- ST
-			  move when "0101", --MOVI i MOVHI
-			  branch when "0110", -- BZ & BNZ
-			  mul_div when "1000", --MUL & DIV
-			  jump when "1010", --JAL
-		      LDB_I when "1101", --LDB
-			  STB_I when "1110", -- STB
-			  HALT_I when "1111", -- HALT
+		op <= arit_log when OP_ARIT, --ARIT_LOGIC
+			  cmp when OP_CMP, --CMP
+			  ADDI_I when OP_ADDI, -- ADDI
+			  LD_I when OP_LD, --LD
+			  ST_I when OP_ST, -- ST
+			  move when OP_MOV, --MOVI i MOVHI
+			  branch when OP_BRANCH, -- BZ & BNZ
+			  mul_div when OP_MULDIV, --MUL & DIV
+			  jump when OP_JUMP, --JAL
+		      LDB_I when OP_LDB, --LDB
+			  STB_I when OP_STB, -- STB
+			  HALT_I when OP_SPECIAL, -- HALT
 			  NOP_I when others;
 
 	with ir(15 downto 12) select
-		Rb_N <= '1' when "0010", --ADDI
-				'1' when "0011", --LD	
-				'1' when "0100", --ST
-				'1' when "0101", --MOVI i MOVHI
-				'1' when "1101", --LDB
-				'1' when "1110", --STB
+		Rb_N <= '1' when OP_ADDI, --ADDI
+				'1' when OP_LD, --LD	
+				'1' when OP_ST, --ST
+				'1' when OP_MOV, --MOVI i MOVHI
+				'1' when OP_LDB, --LDB
+				'1' when OP_STB, --STB
 				'0' when others;
 
 	with ir(15 downto 12) select
-		addr_a <= ir(11 downto 9) when "0101",
+		addr_a <= ir(11 downto 9) when OP_MOV,
 					 ir(8 downto 6) when others;
 
 	addr_d <= ir(11 downto 9);
 	with ir(15 downto 12) select
-		addr_b <= ir(11 downto 9) when "0100",
-					 ir(11 downto 9) when "1110",
-					 ir(11 downto 9) when "0110",
-					 ir(11 downto 9) when "0111",
-					 ir(11 downto 9) when "1010",
+		addr_b <= ir(11 downto 9) when OP_ST,
+					 ir(11 downto 9) when OP_STB,
+					 ir(11 downto 9) when OP_BRANCH,
+					 ir(11 downto 9) when OP_IO,
+					 ir(11 downto 9) when OP_JUMP,
 					 ir(2 downto 0) when others;
 
 	with ir(15 downto 12) select
-		immed <= ir(7) & ir(7) & ir(7) & ir(7) & ir(7) & ir(7) & ir(7) & ir(7) & ir(7 downto 0) when "0101",
+		immed <= ir(7) & ir(7) & ir(7) & ir(7) & ir(7) & ir(7) & ir(7) & ir(7) & ir(7 downto 0) when OP_MOV,
 					ir(5) & ir(5) & ir(5) & ir(5) & ir(5) & ir(5) & ir(5) & ir(5) & ir(5) & ir(5) & ir(5 downto 0) when others;
 
 					
@@ -126,15 +130,15 @@ BEGIN
 					  '0' when others;
 					
 
-	wrd <= '1' when ir(15 downto 12) = "0000" else --op arit
-			 '1' when ir(15 downto 12) = "0001" else --cmp
-			 '1' when ir(15 downto 12) = "0010" else --addi
-			 '1' when ir(15 downto 12) = "0011" else --ld
-			 '1' when ir(15 downto 12) = "0101" else --movi movhiº
-			 '1' when ir(15 downto 12) = "1000" else -- mul & div
-			 jump_wd when ir(15 downto 12) = "1010" else --jal
-			 '1' when ir(15 downto 12) = "1101" else --ldb
-			 '1' when ir(15 downto 12) = "0111" and ir(8) = '0' else --in
+	wrd <= '1' when ir(15 downto 12) = OP_ARIT else --op arit
+			 '1' when ir(15 downto 12) = OP_CMP else --cmp
+			 '1' when ir(15 downto 12) = OP_ADDI else --addi
+			 '1' when ir(15 downto 12) = OP_LD else --ld
+			 '1' when ir(15 downto 12) = OP_MOV else --movi movhiÂº
+			 '1' when ir(15 downto 12) = OP_MULDIV else -- mul & div
+			 jump_wd when ir(15 downto 12) = OP_JUMP else --jal
+			 '1' when ir(15 downto 12) = OP_LDB else --ldb
+			 '1' when ir(15 downto 12) = OP_IO and ir(8) = '0' else --in
 			 '0';
 					
 	--with ir(15 downto 12) select
@@ -149,13 +153,13 @@ BEGIN
 			   --'0' when others;
 
 	 with ir(15 downto 12) select
-		wr_m <= '1' when "0100",
-				  '1' when "1110",
+		wr_m <= '1' when OP_ST,
+				  '1' when OP_STB,
 				  '0' when others;
 				  
 	with ir(15 downto 12) select
-		word_byte <= '1' when "1101",
-						 '1' when "1110",
+		word_byte <= '1' when OP_LDB,
+						 '1' when OP_STB,
 						 '0' when others;
 	
 	with (ir) select
@@ -163,15 +167,15 @@ BEGIN
 				  '1' when others;
 				  
 	with ir(15 downto 12) select
-		immed_x2 <= '1' when "0011",
-						'1' when "0100",
-						'1' when "0110",
+		immed_x2 <= '1' when OP_LD,
+						'1' when OP_ST,
+						'1' when OP_BRANCH,
 						'0' when others;
 		
-	in_d <= "01" when ir(15 downto 12) = "0011" else --st
-			  "01" when ir(15 downto 12) = "1101" else --stb
-			  "10" when ir(15 downto 12) = "1010" else --jal
-			  "11" when ir(15 downto 12) = "0111" and ir(8) = '0' else --in
+	in_d <= "01" when ir(15 downto 12) = OP_ST else --st
+			  "01" when ir(15 downto 12) = OP_STB else --stb
+			  "10" when ir(15 downto 12) = OP_JUMP else --jal
+			  "11" when ir(15 downto 12) = OP_IO and ir(8) = '0' else --in
 			  "00";
 		
 	--with ir(15 downto 12) select -- ara in_d te dos bits
@@ -180,10 +184,10 @@ BEGIN
 				--  "10" when "1010", --jal
 				  --"00" when others;
 	
-	wr_out <= '1' when ir(15 downto 12) = "0111" and ir(8) = '1'  else --OUT
+	wr_out <= '1' when ir(15 downto 12) = OP_IO and ir(8) = '1'  else --OUT
 				 '0';
 
-	rd_in <= '1' when ir(15 downto 12) = "0111" and ir(8) = '0' else --IN
+	rd_in <= '1' when ir(15 downto 12) = OP_IO and ir(8) = '0' else --IN
 				'0';
 	
 	addr_io <= ir(7 downto 0);
