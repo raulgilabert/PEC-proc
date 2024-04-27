15cacd643567fbdabec24186f4b266bfd19b735e
Pol Saumell Hill <94972560+polsahi@users.noreply.github.com>
2024-04-27 19:20:55 +0200

    PEC pa cuando?

---
+++ renacuajo/Etapa7.1/datapath.vhd
@@ -0,0 +1,134 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+
+LIBRARY work;
+USE work.renacuajo_pkg.all;
+
+
+ENTITY datapath IS
+    PORT (clk      : IN  STD_LOGIC;
+          op        : IN INST;
+          wrd      : IN  STD_LOGIC;
+          addr_a   : IN  STD_LOGIC_VECTOR(2 DOWNTO 0);
+          addr_b   : IN  STD_LOGIC_VECTOR(2 DOWNTO 0);
+          addr_d   : IN  STD_LOGIC_VECTOR(2 DOWNTO 0);
+          immed    : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
+          immed_x2 : IN  STD_LOGIC;
+          datard_m : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
+          ins_dad  : IN  STD_LOGIC;
+          pc       : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
+          in_d     : IN  STD_LOGIC_VECTOR(1 DOWNTO 0);
+		  Rb_N     : IN  STD_LOGIC;
+		  d_sys	   : IN  STD_LOGIC;					
+	      a_sys	   : IN  STD_LOGIC;
+		  ei 	   : IN  STD_LOGIC;
+		  di 	   : IN  STD_LOGIC;
+		  reti	   : IN  STD_LOGIC;
+		  rd_io	   : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);	
+          addr_m   : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+          data_wr  : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+		  aluout   : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+		  tknbr    : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+		  wr_io    : OUT STD_LOGIC_VECTOR(15 DOWNTO 0));
+END datapath;
+
+
+ARCHITECTURE Structure OF datapath IS
+
+	COMPONENT regfile IS
+		 PORT (clk    : IN  STD_LOGIC;
+				 wrd    : IN  STD_LOGIC;
+				 d      : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
+				 addr_a : IN  STD_LOGIC_VECTOR(2 DOWNTO 0);
+				 addr_b : IN  STD_LOGIC_VECTOR(2 DOWNTO 0);
+				 addr_d : IN  STD_LOGIC_VECTOR(2 DOWNTO 0);
+				 d_sys	: IN  STD_LOGIC;
+				 a_sys	: IN  STD_LOGIC;
+				 ei 	: IN  STD_LOGIC;
+				 di		: IN  STD_LOGIC;
+				 reti	: IN  STD_LOGIC;
+				 a      : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+				 b      : OUT STD_LOGIC_VECTOR(15 DOWNTO 0));
+	END COMPONENT;
+	
+	COMPONENT alu IS
+		 PORT (x  : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
+				 y  : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
+          op        : IN INST;
+				 w  : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+				 z  : OUT STD_LOGIC);
+	END COMPONENT;
+			
+	SIGNAL ra: std_logic_vector(15 downto 0);	
+	SIGNAL rb: std_logic_vector(15 downto 0);	
+	--SIGNAL rd: std_logic_vector(15 downto 0);
+	SIGNAL d: std_logic_vector(15 downto 0);
+	SIGNAL rd_alu: std_logic_vector(15 downto 0);
+	--SIGNAL rd_mem: std_logic_vector(15 downto 0);
+	SIGNAL immed_out: std_logic_vector(15 downto 0);
+	SIGNAL rb_out: std_logic_vector(15 downto 0);
+	SIGNAL z: std_logic;
+	SIGNAL new_pc: std_logic_vector(15 downto 0);
+BEGIN
+
+	reg0: regfile
+		PORT map(
+			clk => clk,
+			wrd => wrd,
+			d => d,
+			addr_a => addr_a,
+			addr_b => addr_b,
+			addr_d => addr_d,
+			a => ra,
+			b => rb,
+			d_sys => d_sys,
+			a_sys => a_sys,
+			ei => ei,
+			di => di,
+			reti => reti
+		);
+		
+	alu0: alu
+		PORT map(
+			x => ra,
+			y => rb_out,
+			op => op,
+			w => rd_alu,
+			z => z
+		);
+
+	new_pc <= std_logic_vector(unsigned(pc) + 2);
+		
+	with in_d select
+		d <= rd_alu when "00",
+			  new_pc when "10",
+			  rd_io	when "11",
+			  datard_m  when others;
+				
+	with ins_dad select
+		addr_m <= pc when '0',
+					 rd_alu when others;
+					 
+	with immed_x2 select
+		immed_out <= immed when '0',
+						 immed(14 downto 0) & '0' when others;
+	
+	data_wr <= rb;
+
+	with Rb_N select
+		rb_out <= rb when '0',
+				  immed_out when others; 
+
+	aluout <= rd_alu;
+	
+	tknbr(1) <= z when op = BZ_I or op = JZ_I else
+				not z when op = BNZ_I or op = JNZ_I else
+				'1' when op = JMP_I or op = JAL_I else
+				'0';
+
+	tknbr(0) <= '1' when (op = BZ_I or op = BNZ_I) else '0';
+	
+	wr_io <= rb;
+	
+END Structure;
---
+++ renacuajo/Etapa7.1/multi.vhd
@@ -0,0 +1,68 @@
+library ieee;
+USE ieee.std_logic_1164.all;
+
+entity multi is
+    port(clk       : IN  STD_LOGIC;
+         boot      : IN  STD_LOGIC;
+         ldpc_l    : IN  STD_LOGIC;
+         wrd_l     : IN  STD_LOGIC;
+         wr_m_l    : IN  STD_LOGIC;
+         w_b       : IN  STD_LOGIC;
+         ldpc      : OUT STD_LOGIC;
+         wrd       : OUT STD_LOGIC;
+         wr_m      : OUT STD_LOGIC;
+         ldir      : OUT STD_LOGIC;
+         ins_dad   : OUT STD_LOGIC;
+         word_byte : OUT STD_LOGIC);
+end entity;
+
+architecture Structure of multi is
+
+    -- Aqui iria la declaracion de las los estados de la maquina de estados
+		TYPE state_t is (F, DEMW, SYSTEM);
+    SIGNAL state: state_t;
+
+begin
+
+    -- Aqui iria la mï¿½quina de estados del modelos de Moore que gestiona el multiciclo
+    -- Aqui irian la generacion de las senales de control que su valor depende del ciclo en que se esta.
+    PROCESS (clk)
+    BEGIN
+        if rising_edge(clk) then 
+            if boot = '1' then 
+                state <= F;
+            elsif state = F then 
+					state <= DEMW;
+				else 
+					state <= F;
+            END if;
+        END if;
+    END PROCESS;
+	
+	PROCESS (state)
+	BEGIN
+		case state is
+            when F =>   
+                ldpc <= '0';
+                ldir <= '1';
+                wrd <= '0';
+                wr_m <= '0';
+                word_byte <= '0';
+					 ins_dad <= '0';
+            when DEMW =>
+                ldir <= '0';
+                ins_dad <= '1';
+                wrd <= wrd_l;
+                wr_m <= wr_m_l;
+                word_byte <= w_b;
+                ldpc <= ldpc_l;
+				when SYSTEM => 
+					--res
+		END case;
+	END PROCESS;
+				
+
+        
+
+
+end Structure;
